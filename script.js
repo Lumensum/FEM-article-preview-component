66a53d35a48e0f4441136dfc2668f00f352ab91f
Lumensum <roy.schrauwen@gmail.com>
2021-08-03 23:05:35 +0200

    Share Menu Fading In

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const shareButton = document.getElementById("shareButton");
+const shareButton2 = document.getElementById("shareButton2");
 const shareBar = document.querySelector(".main__share");
 const authorBar = document.querySelector(".author");
 
 shareButton.addEventListener("click", () => {
-  if (shareBar.style.display == "block") {
-    shareBar.style.display = "none";
+  if (shareBar.style.bottom == "-60px") {
+    shareBar.style.bottom = "-150px";
   } else {
-    shareBar.style.display = "block";
+    shareBar.style.bottom = "-60px";
+  }
+});
+
+shareButton2.addEventListener("click", () => {
+  if (shareBar.style.bottom == "-60px") {
+    shareBar.style.bottom = "-150px";
+  } else {
+    shareBar.style.bottom = "-60px";
   }
 });
