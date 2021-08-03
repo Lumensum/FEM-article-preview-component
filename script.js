3d904a5b1c738d94649dcff6cb6b675018c086ad
Lumensum <roy.schrauwen@gmail.com>
2021-08-03 17:50:57 +0200

    Startming Meaid Query Desktop

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const shareButton = document.getElementById("shareButton");
+const shareBar = document.querySelector(".main__share");
+const authorBar = document.querySelector(".author");
 
 shareButton.addEventListener("click", () => {
-  console.log("Sharebutton clicked");
+  if (shareBar.style.display == "block") {
+    shareBar.style.display = "none";
+  } else {
+    shareBar.style.display = "block";
+  }
 });
