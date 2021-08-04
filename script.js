a8dc55f7db7237c5faf140e1aba669a50b3f1348
Lumensum <roy.schrauwen@gmail.com>
2021-08-04 21:31:00 +0200

    sharbar in div of author

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const shareButton = document.getElementById("shareButton");
 const shareButton2 = document.getElementById("shareButton2");
 const shareBar = document.querySelector(".sharebar");
-const authorBar = document.querySelector(".authorbar");
 
 shareButton.addEventListener("click", toggleSharebar);
 shareButton2.addEventListener("click", toggleSharebar);
